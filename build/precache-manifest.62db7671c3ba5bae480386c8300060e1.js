c87781d9e7145a61ccf8fe6d3dfe75357e8df485
Gerardo Santos Gonzalez <65302754+gerardosantosg@users.noreply.github.com>
2020-08-27 17:24:38 -0500

    Add files via upload

---
+++ build/precache-manifest.62db7671c3ba5bae480386c8300060e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319e235430934c96ea7394290f14b433",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31a4cdc8623d18004d95",
+    "url": "/static/css/main.6609778b.chunk.css"
+  },
+  {
+    "revision": "139373b582e86128d6c0",
+    "url": "/static/js/2.2e37099c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2e37099c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a4cdc8623d18004d95",
+    "url": "/static/js/main.2ac20c65.chunk.js"
+  },
+  {
+    "revision": "89555f4742fa99b9a2c9",
+    "url": "/static/js/runtime-main.a146df55.js"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/bblogo.3795f66d.png"
+  },
+  {
+    "revision": "29b76ffe308a741272f7001a41e85ecb",
+    "url": "/static/media/spinnerbb.29b76ffe.gif"
+  }
+]);
